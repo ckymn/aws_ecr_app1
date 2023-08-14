981d5ad52cf90322db770fe842f61a3b5c335b93
ckymn <cokyamanmuhammet@gmail.com>
2023-08-14 21:03:01 +0300

    done

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIA2K2QTFYVHFFJA6WM"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
